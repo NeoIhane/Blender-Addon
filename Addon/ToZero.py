24ccb152c34d31eff668032e6eb808eff8235348
neoihane <orrumpha.wongsripaisan@gmail.com>
2018-01-26 10:18:16 +0700

    add menu_func

--- Addon/ToZero.py
+++ Addon/ToZero.py
@@ -1,6 +1,6 @@
 bl_info = {
     "name": "ToZero",
-    "category": "T",
+    "category": "Objects",
 }
 
 import bpy
@@ -24,14 +24,23 @@ class ObjectToZero(bpy.types.Operator):
         obj.location.z=0;
         
         return {'FINISHED'}
+    
+def menu_func(self, context):
+    self.layout.operator(ObjectToZero.bl_idname)
 
 def register():
     bpy.utils.register_class(ObjectToZero)
-
+    bpy.types.VIEW3D_MT_object.append(menu_func)
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
 
 def unregister():
-    bpy.utils.unregister_class(ObjectToZero)
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
 
+    bpy.utils.unregister_class(ObjectToZero)
+    bpy.types.VIEW3D_MT_object.remove(menu_func)
 
 if __name__ == "__main__":
     register()
